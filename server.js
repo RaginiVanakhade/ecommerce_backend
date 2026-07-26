b5020888c9f060b66e1cd36ac6c1757608e9a031
rohan-mahabare-fgli <g.rohan.mahabare50@generalicentral.com>
2026-07-26 19:32:59 +0530

    Fix render deployment

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 require("dotenv").config();
+
 const app = require("./src/app");
 const connectDB = require("./src/config/db");
 
-connectDB();
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
-});
+const PORT = process.env.PORT || 5000;
+
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to start server:", error);
+  }
+};
+
+startServer();
