61b4011eeda6476617766c0ca09c53bd502bca0d
rohan-mahabare-fgli <g.rohan.mahabare50@generalicentral.com>
2026-08-11 18:06:44 +0530

    fix cors error

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-console.log("App Loaded");
+const cors = require("cors");
 const userRoutes = require("./routes/user.routes");
 const authRoutes = require("./routes/auth.routes");
 const productRoutes = require("./routes/product.routes");
@@ -10,6 +10,14 @@ const checkoutRoutes = require("./routes/checkout.routes");
 const paymentRoutes = require("./routes/payment.routes");
 
 const app = express();
+
+// =========================
+// CORS
+// =========================
+app.use(cors());
+
+// =========================
+
 app.use(express.json());
 app.use("/users", userRoutes);
 app.use("/auth", authRoutes);
